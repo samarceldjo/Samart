cfe12620657936b5dc9ef2754d0e5f7881430064
Marcel Sadjo <meldjora95@gmail.com>
2022-03-11 09:35:41 +0000

    Ajout de sidebar-menu et changement de layout products

--- JS/script.js
+++ JS/script.js
@@ -1,53 +1,16 @@
-const imgs = document.querySelectorAll(".slider img");
-const productDetails = document.querySelectorAll(".slider span");
-const imgLength = imgs.length;
-const previous = document.getElementById("btn-1");
-const next = document.getElementById("btn-2");
-let count = 0;
 
-console.log(productDetails);
-function nextImage() {
-    imgs[count].classList.remove('active');
-    productDetails[count].classList.remove('active');
+//  Navigation menu
+const navLinks = document.querySelector(".nav-links");
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
 
-    if (count < imgLength - 1) {
-        count++;
-    } else {
-        count = 0;
-    }
+openMenu.addEventListener("click", () => {
+    navLinks.style.left = 0;
+    navLinks.style.transition = 0.8 + 's';
+});
 
-    imgs[count].classList.add('active');
-    productDetails[count].classList.add('active');
-}
+closeMenu.addEventListener("click", () => {
+    navLinks.style.left = -270 + "px";
+});
 
-function previousImage() {
-    imgs[count].classList.remove('active');
-    productDetails[count].classList.remove('active');
-
-    if (count > 0) {
-        count--;
-    } else {
-        count = imgLength - 1;
-    }
-
-    imgs[count].classList.add('active');
-    productDetails[count].classList.add('active');
-}
-
-next.addEventListener("click", nextImage);
-previous.addEventListener("click", previousImage);
-
-
-setInterval(() => {
-    imgs[count].classList.remove('active');
-    productDetails[count].classList.remove('active');
-
-    if (count < imgLength - 1) {
-        count++;
-    } else {
-        count = 0;
-    }
-
-    imgs[count].classList.add('active');
-    productDetails[count].classList.add('active');
-}, 5000);
+//---------------------------------------------------------
